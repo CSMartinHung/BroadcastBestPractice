41d3b8546273b0d2f6a3d1001d0bd2296d23cf0b
CSMartinHung <329303422@qq.com>
2017-04-07 15:42:13 +0800

    First commit.

---
+++ app/src/main/java/com/martin/broadcastbestpractice/LoginActivity.java
@@ -0,0 +1,66 @@
+package com.martin.broadcastbestpractice;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+/**
+ * Created by csmartin on 17/4/2.
+ */
+public class LoginActivity extends BaseActivity {
+
+    private EditText accountEdit;
+    private EditText passwordEdit;
+    private Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+        accountEdit = (EditText)findViewById(R.id.account);
+        passwordEdit = (EditText)findViewById(R.id.password);
+        login = (Button)findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String account = accountEdit.getText().toString();
+                String password = passwordEdit.getText().toString();
+                //如果账号是admin且密码是123456,就认为登录成功
+                if (account.equals("admin") && password.equals("123456")) {
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Toast.makeText(LoginActivity.this, "account or password is invalid", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        //API24相对API19,权限方面有所变动
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (!Settings.canDrawOverlays(LoginActivity.this)) {
+                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName()));
+                startActivityForResult(intent, 10);
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 10) {
+            if (Build.VERSION.SDK_INT >= 23) {
+                if (!Settings.canDrawOverlays(this)) {
+                    //SYSTEM_ALERT_WINDOW permission not granted...
+                    Toast.makeText(LoginActivity.this, "not granted", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+}
